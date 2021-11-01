74c68dd5eaa896712f2d356fdfd59df28431069d
ANNI310 <85473727+ANNI310@users.noreply.github.com>
2021-11-01 14:32:05 +0530

    first commit

---
+++ change.js
@@ -0,0 +1,14 @@
+document.getElementById("services").addEventListener("mouseover", function() {
+    document.getElementById("services").style.backgroundColor = "grey";
+});
+    
+document.getElementById("services").addEventListener("mouseout", function() {
+    document.getElementById("services").style.backgroundColor = "white";
+});
+document.getElementById("myID").addEventListener("mouseover", function() {
+    document.getElementById("myID").style.backgroundColor = "black";
+});
+    
+document.getElementById("myID").addEventListener("mouseout", function() {
+    document.getElementById("myID").style.backgroundColor = "white";
+});

adfdf66639687f339cb1b7d2ff7f0507a08d130b
ANNI310 <85473727+ANNI310@users.noreply.github.com>
2021-11-01 18:32:20 +0530

    task2

--- change.js
+++ change.js
@@ -12,3 +12,15 @@ document.getElementById("myID").addEventListener("mouseover", function() {
 document.getElementById("myID").addEventListener("mouseout", function() {
     document.getElementById("myID").style.backgroundColor = "white";
 });
+var $Osc = {
+    hover: function(event) {
+      event.target.style.backgroundColor = "blue";
+    },
+    out: function(event) {
+      event.target.style.backgroundColor = "white";
+    }
+  
+  }
+  var $OscElement = document.getElementById("col-lg-4 col-md-6 box wow bounceInUp");
+  $OscElement.addEventListener("mouseover", $Osc.hover, false);
+  $OscElement.addEventListener("mouseout", $Osc.out, false);
